f28e029fb12acb02a769cd1c4c2e9b2666081a4b
MattiDragon <matias.koivikko@gmail.com>
2026-06-09 23:22:00 +0300

    Add noctalia settings toml (not yet integrated)

--- modules/parts/desktop/niri/noctalia/default.nix
+++ modules/parts/desktop/niri/noctalia/default.nix
@@ -46,7 +46,6 @@
         });
 
         settings = {
-          # This may also be a string or path to a .toml file.
           theme = {
             mode = "dark";
             source = "builtin";
