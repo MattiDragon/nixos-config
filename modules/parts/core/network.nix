7524486fefd4e5784740eea15c512900513c5d53
MattiDragon <matias.koivikko@gmail.com>
2026-09-06 19:01:33 +0300

    Add openvpn support

--- modules/parts/core/network.nix
+++ modules/parts/core/network.nix
@@ -1,7 +1,10 @@
 {
   flake.modules.nixos.core =
-    { ... }:
+    { pkgs, ... }:
     {
       networking.networkmanager.enable = true;
+      networking.networkmanager.plugins = with pkgs; [
+        networkmanager-openvpn
+      ];
     };
 }
